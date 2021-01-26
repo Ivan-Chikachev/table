9dd036fcb9dd3ec6eb317c4f22ae715189321b37
Ivan Ch <ivan-chikachev@mail.ru>
2021-01-26 10:48:16 +0300

    test

--- src/api/PersonAPI.js
+++ src/api/PersonAPI.js
@@ -2,11 +2,11 @@ import * as axios from 'axios';
 
 const instanceSmall = axios.create({
 	baseURL:
-		'http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}'
+		'https://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}'
 });
 const instanceBig = axios.create({
 	baseURL:
-		'http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}'
+		'https://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}'
 });
 
 export const personsAPI = {
